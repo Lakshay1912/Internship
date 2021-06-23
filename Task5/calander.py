29960b78de9d1ef5c364a9f02787bfc85f7bfee8
Lakshay Vasuja <47741561+Lakshay1912@users.noreply.github.com>
2021-06-23 16:31:59 +0530

    Add files via upload

---
+++ Task5/calander.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Program to display calendar of the given month and year
+
+# importing calendar module
+import calendar
+
+yy = 2021  # year
+mm = 6    # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
