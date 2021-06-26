739dfde915ccf22b69b407371994f944786ca569
Lakshay Vasuja <47741561+Lakshay1912@users.noreply.github.com>
2021-06-26 10:44:47 +0530

    Update MLib.py

--- Task5/MLib.py
+++ Task5/MLib.py
@@ -1,4 +1,3 @@
-# Databricks notebook source
 import pyspark
 from pyspark.sql import SparkSession
 #SparkSession is now the entry point of Spark
@@ -9,14 +8,14 @@ spark=SparkSession.builder.appName('housing_price_model').getOrCreate()
   
 #create spark dataframe of input csv file
 df = spark.read\
-    .format("s3selectCSV")\
-    .load("s3://raniai/cruise_ship_info.csv")
+    .format("csv")\
+    .load("dbfs:/FileStore/shared_uploads/500067177@stu.upes.ac.in/cruise_ship_info.csv", header = True, inferSchema = True)
 
 df.show(10)
 df.printSchema()
 
 from pyspark.ml.feature import StringIndexer
-indexer=StringIndexer(inputCol='_c1',outputCol='cruise_cat')
+indexer=StringIndexer(inputCol='Cruise_line',outputCol='cruise_cat')
 indexed=indexer.fit(df).transform(df)
 
 for item in indexed.head(5):
@@ -27,19 +26,19 @@ from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import VectorAssembler
 #creating vectors from features
 #Apache MLlib takes input if vector form
-assembler=VectorAssembler(inputCols=['_c2',
- '_c3',
- '_c4',
- '_c5',
- '_c6',
- '_c7',
+assembler=VectorAssembler(inputCols=['Age',
+ 'Tonnage',
+ 'passengers',
+ 'length',
+ 'cabins',
+ 'passenger_density',
  'cruise_cat'],outputCol='features')
 output=assembler.transform(indexed)
-output.select('features','_c8').show(5)
+output.select('features','crew').show(5)
 #output as below
 
 #final data consist of features and label which is crew.
-final_data=output.select('features','_c8')
+final_data=output.select('features','crew')
 #splitting data into train and test
 train_data,test_data=final_data.randomSplit([0.7,0.3])
 train_data.describe().show()
